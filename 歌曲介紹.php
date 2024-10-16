202d729bca046ce28770bbd5b83a6f5621e69c52
user638726 <lienjackson35@gmail.com>
2024-10-16 15:51:12 +0800

    增加歌曲介紹

---
+++ 歌曲介紹.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="referrer" content="strict-origin-when-cross-origin" />
+    <title>Document</title>
+    <style>
+        table{
+            background: white;
+            width:100%;
+            text-align: center;
+            margin: 0 auto;
+            border-collapse: collapse;
+        }
+        
+        tr,td{
+            border:1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Way down We Go</h1>
+    <p>KALEO</p>
+    <p>作詞：DAVID NILS OLOF ANTONSSON, JOKULL JULIUSSON, DANIEL AEGIR KRISTJANSSON, RUBIN POLLOCK     作曲：DAVID NILS OLOF ANTONSSON, JOKULL JULIUSSON, DANIEL AEGIR KRISTJANSSON, RUBIN POLLOCK</p>
+    <table>
+        <tr>
+            <td><img src="https://i.kfs.io/album/global/7355012,6v1/fit/500x500.jpg"></td>
+            <td>
+                <pre>
+                Ohh father tell me
+                Do we get what we deserve
+                We get what we deserve
+                
+                Way down we go
+                Way down we go
+                Way down we go
+                Way down we go
+                
+                You let your feet run wild
+                The time has come as we all go down
+                Ohh before the fall
+                Do you dare to look them right in the eyes?
+                
+                'Cause they will run you down
+                Down to the dark
+                Yes and they will run you down
+                Down 'till you fall
+                And they will run you down
+                Down to your core
+                'till you can't crawl no more
+                
+                And way down we go
+                Way down we go
+                Way down we go
+                'Cause they will run you down
+                Down till you're caught
+                Ohh way down we go
+                
+                Way down we go
+                Way down we go
+                Way down we go
+                Way down we go
+            </pre>
+            </td>
+        </tr>
+    </table>
+    
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.703506716216!2d121.41694067595641!3d25.04413413788237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a7bec9ad74b1%3A0xa639904a89f26435!2z5Yue5YuV6YOo5Yue5YuV5Yqb55m85bGV572y5YyX5Z-65a6c6Iqx6YeR6aas5YiG572y5rOw5bGx6IG35qWt6KiT57e05aC0!5e0!3m2!1szh-TW!2stw!4v1729059297365!5m2!1szh-TW!2stw" width="500" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/0-7IHOXkiV8?si=4GsmbDM2ubiUgPZY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</body>
+</html>
