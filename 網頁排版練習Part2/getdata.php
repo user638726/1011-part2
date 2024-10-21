81b13c1c98764e7fbc88fa483b00b11f59130772
user638726 <lienjackson35@gmail.com>
2024-10-21 11:13:10 +0800

    增加./getdata.php

---
+++ 網頁排版練習Part2/getdata.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    function dd($data){
+        print('<pre>');
+        print_r($data);
+        print('</pre>');
+    }
+
+    // $_GET form 傳過來的資料
+    $data = $_GET;
+
+    // $data = [
+    //     's1' => 'amy',
+    //     's2' => 'amy',
+    //     's3' => 'amy'
+    // ];
+    dd($data);
+?>
+</body>
+</html>
