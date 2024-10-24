aa9cbbe5ffca661c3467c0067e4b1d0f98e3920f
user638726 <lienjackson35@gmail.com>
2024-10-24 09:03:38 +0800

    增加新的字串練習

---
+++ 字串處理/字串part1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>字串</title>
+</head>
+<body>
+    <?php 
+    echo str_replace("aadw1123","*********","aadw1123")
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
