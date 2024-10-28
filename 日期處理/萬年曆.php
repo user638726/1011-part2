b8bd275cd97157258d1bffa107f5c7daf265ba74
user638726 <lienjackson35@gmail.com>
2024-10-28 16:08:48 +0800

    萬年曆

---
+++ 日期處理/萬年曆.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>萬年曆</title>
+    <style>
+     table{
+        table border-collapse:collapse;
+    }
+     td{
+        padding:5px 10px;
+        text-align: center;
+        border:1px solid #999;
+    }
+    .holiday{
+        background:pink;
+        color:#999;
+    }
+    .grey-text{
+        color:#999;
+    }
+    .today{
+        background:blue;
+        color:white;
+        font-weight:bolder;
+    }
+</style>
+</head>
+<body>
+<h3><?php echo date("m月");?></h3>
+<ul>
+    <li>有上一個月下一個月的按鈕</li>
+    <li>萬年曆都在同一個頁面同一個檔案</li>
+    <li>有前年和來年的按鈕</li>
+</ul>
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+<a href="">前年</a>
+<a href="">上一個月</a>
+<a href="">下一個月</a>
+<a href="">明年</a>
+<table>
+<tr>
+    <td></td>
+    <td>日</td>
+    <td>一</td>
+    <td>二</td>
+    <td>三</td>
+    <td>四</td>
+    <td>五</td>
+    <td>六</td>
+</tr>
+<?php
+$firstDay=date("Y-m-1");
+$firstDayTime=strtotime($firstDay);
+$firstDayWeek=date("w",strtotime(date("Y-m-1")));
+
+for($i=0;$i<6;$i++){
+    echo "<tr>";
+    echo "<td>";
+    echo $i+1;
+    echo "</td>";
+    for($j=0;$j<7;$j++){
+        //echo "<td class='holiday'>";
+        $cell=$i*7+$j -$firstDayWeek;
+        $theDayTime=strtotime("$cell days".$firstDay);
+
+        //所需樣式css判斷
+        $theMonth=(date("m",$theDayTime)==date("m"))?'':'grey-text';
+        $isToday=(date("Y-m-d",$theDayTime)==date("Y-m-d"))?'today':'';
+        $w=date("w",$theDayTime);
+        $isHoliday=($w==0 || $w==6)?'holiday':'';
+        
+        echo "<td class='$isHoliday $theMonth $isToday'>";
+        echo date("d",$theDayTime);
+        echo "</td>";
+        
+    }
+    echo "</tr>";
+}
+
+
+
+?>
+</body>
+</html>
