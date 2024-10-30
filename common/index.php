63080aabba84fc79f51dc4db86ee6e341a555c17
user638726 <lienjackson35@gmail.com>
2024-10-30 16:09:43 +0800

    修改資料夾名稱

--- common/index.php
+++ common/index.php
@@ -8,11 +8,7 @@
 </head>
 <body>
     <h1>首頁</h1>
-    <a href="about.php">關於我們</a>
-    <a href="product.php">產品介紹</a>
-    <a href="contact.php">聯絡我們</a>
-    <a href="login.php">登入</a>
-
+        <?php include("common/bar.php")?>
     <main>
         <h2>歡迎光臨</h2>
         <p>這是一個網站的首頁</p>
