0085a8cfed5cfc3ac08e89dc55b5801c8021edf2
user638726 <lienjackson35@gmail.com>
2024-11-01 09:00:51 +0800

    增加註解

--- include/bar.php
+++ include/bar.php
@@ -10,7 +10,14 @@ $header=[
 ];
 
 ?>
-
+<!---
+    <a class="<?=($page=='index')?'now-page':'';?>" href="index.php">首頁</a>
+    <a class="<?=($page=='about')?'now-page':'';?>" href="about.php">關於我們</a>
+    <a class="<?=($page=='product')?'now-page':'';?>" href="product.php">產品介紹</a>
+    <a class="<?=($page=='contact')?'now-page':'';?>" href="contact.php">聯絡我們</a>
+    <a class="<?=($page=='login')?'now-page':'';?>" href="login.php">登入</a>
+-->
+    */
 <h1><?=$header[$page];?></h1>
 <?php
 foreach($header as $key => $value){
