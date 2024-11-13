7b8545c7ed55847d6e914f1773cc78e4732454a2
user638726 <lienjackson35@gmail.com>
2024-11-13 16:02:41 +0800

    增加

--- SQL與PHP的練習/login/success.php
+++ SQL與PHP的練習/login/success.php
@@ -6,6 +6,44 @@
     <title>Document</title>
 </head>
 <body>
-<h1>登入成功</h1>
+    <h1>登入成功</h1>
+
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=crud";
+$pdo=new PDO($dsn,'root','');
+
+$rows=$pdo->query("select * from member")->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<table>
+    <tr>
+        <td>id</td>
+        <td>acc</td>
+        <td>pw</td>
+        <td>email</td>
+        <td>tel</td>
+        <td>操作</td>
+    </tr>
+    <?php
+foreach($rows as  $row){
+
+?>
+    <tr>
+        <td><?=$row['id'];?></td>
+        <td><?=$row['acc'];?></td>
+        <td><?=$row['pw'];?></td>
+        <td><?=$row['email'];?></td>
+        <td><?=$row['tel'];?></td>
+        <td>
+            <a href="login2.php">編輯</a>
+            <a href="del.php?id=<?=$row['id'];?>">刪除</a>
+        </td>
+    </tr>
+    <?php
+
+}
+?>
+</table>
 </body>
 </html>
