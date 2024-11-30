db811b625ae5a5f2c739cdd13d8f1245d26384b6
user638726 <lienjackson35@gmail.com>
2024-11-30 18:09:08 +0800

    改

--- 字串處理/字串part1.php
+++ 字串處理/字串part1.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,25 +17,4 @@
     
     ?>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>字串</title>
-</head>
-<body>
-    <?php 
-    echo str_replace("aadw1123","********","aadw1123");
-    echo "<br>";
-    echo str_replace(['a','a','d','w','1','2','3'],"*",'aaddw123');
-    echo "<br>";
-    echo str_repeat("*",mb_strlen('aaddw123'));
-    
-    
-    
-    ?>
-</body>
->>>>>>> 4a934a5dc1e68a9427d400dc0cc4ad9eb6aebac5
 </html>
--- 陣列練習/arraypart8.php
+++ 陣列練習/arraypart8.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,32 +24,4 @@ for($i=2024;$i<=2524;$i++){
 
 
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<?php
-$leap=[];
-
-for($i=2024;$i<=2524;$i++){
-    if($i % 4==0 && ($i % 100 != 0 || $i % 400 == 0)){
-        $leap[]=$i;
-
-    }
-    echo "<pre>";
-    print_r($leap);
-    echo "</pre>"; 
-
-}
-
-?> 
-
-
-</body>
->>>>>>> 4a934a5dc1e68a9427d400dc0cc4ad9eb6aebac5
 </html>
