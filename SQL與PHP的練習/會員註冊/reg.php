d4368fc0ae1d9bb935a3710ff8aedcb56ee32444
user638726 <lienjackson35@gmail.com>
2024-11-30 17:58:48 +0800

    改

--- SQL與PHP的練習/會員註冊/reg.php
+++ SQL與PHP的練習/會員註冊/reg.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
    
    $sql="insert into `member`(`acc`,`pw`,`email`,`tel`) values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['email']}','{$_POST['tel']}')";
@@ -15,22 +14,4 @@
    
    }
    
-=======
-<?php
-   
-   $sql="insert into `member`(`acc`,`pw`,`email`,`tel`) values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['email']}','{$_POST['tel']}')";
-   $dsn="mysql:host=localhost;charset=utf8;dbname=crud";
-   $pdo=new PDO($dsn,'root','');
-   
-   if($pdo->exec($sql)){
-   
-       header("location:reg_form.php?status=1");
-   
-   }else{
-   
-       header("location:reg_form.php?status=0");
-   
-   }
-   
->>>>>>> 4a934a5dc1e68a9427d400dc0cc4ad9eb6aebac5
 ?>
--- php練習/9x9乘法表.php
+++ php練習/9x9乘法表.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,57 +49,4 @@
  ?>
 
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    echo "<table border=1>";
-      for($i=1;$i<=9;$i++)
-      {
-        echo "<tr>";
-        for($j=1;$j<=9;$j++){
-        echo "<td>";    
-        echo "$i x $j = ".($i*$j);
-        echo "</td>";
-        }
-        echo "</tr>";
-      }
-      echo "</table>";
-      echo "<br>";
-    
-    echo "<table border=1>"; 
-   
-        for($i=0;$i<=9;$i++){
-            echo "<tr>";
-            
-        for($j=0;$j<=9;$j++){
-            echo "<td>";
-            if($j==0 && $i==0){
-                echo "";
-            }
-            elseif ($i==0) {
-                echo "$j";
-            }
-            elseif($j == 0){
-                echo "$i";
-            }else{
-                echo ($i*$j);
-            }
-        
-            echo "</td>";
-        }
-         echo "</tr>";
-        }
-      echo "</table>";
-
- ?>
-
-</body>
->>>>>>> 4a934a5dc1e68a9427d400dc0cc4ad9eb6aebac5
 </html>
